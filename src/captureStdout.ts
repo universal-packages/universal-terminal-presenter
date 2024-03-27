e0edba2282813ed26917698012e5ec9d9239ceed
David De Anda <omarandstuff@gmail.com>
2024-03-27 13:51:20 -0600

    Do not try to be the hero with other outputs besides console

--- src/captureStdout.ts
+++ src/captureStdout.ts
@@ -20,18 +20,22 @@ export function pushStdoutWriteAttempt(subject: string): void {
 }
 
 function stdoutCapturer(bufferOrString: Uint8Array | string, ...args: any[]): boolean {
-  const stack = new Error().stack.split('\n')
-  const printerValueStackLineIndex = stack.findIndex((line) => line.includes('node:internal/console/constructor'))
-  const printerStackLine = stack[printerValueStackLineIndex + 1]
-  const printerMatch = /at (.*) .*/g.exec(printerStackLine)
-  const printer = printerMatch?.[1]
-  const callerStackLine = stack[printerValueStackLineIndex + 2]
-  const callerMatchType1 = /at (.*) \((.*)\)/g.exec(callerStackLine)
-  const callerMatchType2 = /at (.*)/g.exec(callerStackLine)
-  const caller = callerMatchType1?.[1]
-  const line = callerMatchType1 ? callerMatchType1[2] : callerMatchType2[1]
-
   if (typeof bufferOrString === 'string') {
+    const stack = new Error().stack.split('\n')
+    const printerValueStackLineIndex = stack.findIndex((line) => line.includes('node:internal/console/constructor'))
+
+    // Console was not the caller
+    if (printerValueStackLineIndex === -1) return ORIGINAL_STDOUT_WRITE(bufferOrString, ...args)
+
+    const printerStackLine = stack[printerValueStackLineIndex + 1]
+    const printerMatch = /at (.*) .*/g.exec(printerStackLine)
+    const printer = printerMatch?.[1]
+    const callerStackLine = stack[printerValueStackLineIndex + 2]
+    const callerMatchType1 = /at (.*) \((.*)\)/g.exec(callerStackLine)
+    const callerMatchType2 = /at (.*)/g.exec(callerStackLine)
+    const caller = callerMatchType1?.[1]
+    const line = callerMatchType1 ? callerMatchType1[2] : callerMatchType2[1]
+
     STDOUT_WRITE_ATTEMPTS.push({ subject: bufferOrString, caller, printer, line })
     return true
   } else {
